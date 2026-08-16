05a75c095c658bad8270c155cf0b5e55e9da18bf
Stephen Ferrell <sferrellblue@yahoo.com>
2026-08-15 19:49:48 -0600

    Delete gridview.h

--- gridview.h
+++
@@ -1,8 +0,0 @@
-void GenGridOnView();
-void CropGridOnView();
-void CopyGridToSpeed();
-void CopyGridToColour();
-void CopyGridToDirection();
-void KillLayerGrids();
-void ColourLayerCompare();
-void ColourLayerVolume();
